a1e735760f78e8127e666e54ff8f97e9741974c5
Ramazan Sakin <ramazansakin63@gmail.com>
2021-01-21 21:20:16 +0300

    password validation annotation impl

--- src/main/java/id/test/springboottesting/model/User.java
+++ src/main/java/id/test/springboottesting/model/User.java
@@ -1,11 +1,13 @@
 package id.test.springboottesting.model;
 
+import id.test.springboottesting.util.annotation.ValidPassword;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 /***
@@ -27,11 +29,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Email(message = "Email is not valid")
     @NotEmpty(message = "Email should not be empty")
     @Column(nullable = false, unique = true, length = 100)
     private String email;
 
     @Column(nullable = false, length = 100)
+    @ValidPassword
     private String password;
 
     @NotEmpty(message = "Username should not be empty")
